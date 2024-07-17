01d2580063b01514c547da08941734579b38acdb
Th3C4t <kurk070ff@yandex.ru>
2024-07-18 00:24:42 +0300

    Added numbers addition

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
 use nom::{
+    branch::alt,
     bytes::complete::{tag, take_while},
     character::complete::space0,
     combinator::{map_res, opt},
@@ -14,6 +15,11 @@ use rand::Rng;
 pub struct Dice {
     pub num_sides: u32,
     pub quantity: u32,
+}
+
+enum Token {
+    Die(Dice),
+    Num(u32),
 }
 
 impl Dice {
@@ -35,22 +41,27 @@ fn to_num(input: &str) -> Result<u32> {
     Ok(num)
 }
 
-fn die(input: &str) -> IResult<&str, Dice> {
+fn num(input: &str) -> IResult<&str, Token> {
+    let (input, num) = map_res(take_while(char::is_dec_digit), to_num)(input)?;
+    Ok((input, Token::Num(num)))
+}
+
+fn die(input: &str) -> IResult<&str, Token> {
     let (input, quantity) = opt(map_res(take_while(char::is_dec_digit), to_num))(input)?;
     let (input, _) = tag("d")(input)?;
     let (input, num_sides) = map_res(take_while(char::is_dec_digit), to_num)(input)?;
 
     Ok((
         input,
-        Dice {
+        Token::Die(Dice {
             num_sides,
             quantity: quantity.unwrap_or(1),
-        },
+        }),
     ))
 }
 
-fn dice_expression(input: &str) -> IResult<&str, Vec<Dice>> {
-    let (input, dice_vec) = many0(tuple((space0, die, space0, opt(tag("+")))))(input)?;
+fn dice_expression(input: &str) -> IResult<&str, Vec<Token>> {
+    let (input, dice_vec) = many0(tuple((space0, alt((die, num)), space0, opt(tag("+")))))(input)?;
     let dice_res = dice_vec.into_iter().map(|(_, d, _, _)| d).collect();
     Ok((input, dice_res))
 }
@@ -71,8 +82,11 @@ fn main() -> Result<()> {
     let (_, dice_vec) = dice_expression(&expression)
         .map_err(|_| anyhow::format_err!("Failed to parse expression"))?;
     let rolls = dice_vec
-        .into_iter()
-        .map(|die| (die, die.roll()))
+        .iter()
+        .filter_map(|t| match t {
+            Token::Die(die) => Some((die.clone(), die.roll())),
+            _ => None,
+        })
         .collect::<Vec<(Dice, Vec<u32>)>>();
     rolls.iter().for_each(|(die, roll)| {
         println!(
@@ -92,6 +106,13 @@ fn main() -> Result<()> {
                 .into_iter()
                 .map(|(_, roll)| roll.iter().sum::<u32>())
                 .sum::<u32>()
+                + dice_vec
+                    .iter()
+                    .filter_map(|t| match t {
+                        Token::Num(n) => Some(n),
+                        _ => None,
+                    })
+                    .sum::<u32>()
         );
     }
     Ok(())
