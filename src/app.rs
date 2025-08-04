8714a729690b31a897284d0b7697d85ca81bd21f
Freya Pines <one-true-cat@yandex.ru>
2025-08-04 19:37:22 +0500

    refactor: switched app to work with the new crates

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,25 @@
-use std::sync::Arc;
-
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::Parser;
-use nom::Finish;
 
 use crate::cli::CliArgs;
-use rusty_dice_expressions::roll_expressions_list;
-use rusty_dice_expressions::{RollExpression, RollResults};
+use rusty_dice_expressions::{eval::Eval, parse::ExprKind};
 
 pub struct App {
     args: CliArgs,
+}
+
+fn format_expr(expr: &ExprKind) -> Result<String> {
+    let res = match expr {
+        ExprKind::Simple(expr) => format!("{}: {}", expr, expr.clone().eval()?),
+        ExprKind::Labeled(l, expr) => format!("{l}: {}", expr.clone().eval()?),
+        ExprKind::Separated(expr_kinds) => expr_kinds
+            .iter()
+            .map(|e| format_expr(e))
+            .collect::<Result<Vec<_>, _>>()?
+            .join("\n"),
+    };
+
+    Ok(res)
 }
 
 impl App {
@@ -19,68 +29,10 @@ impl App {
         }
     }
 
-    fn parse_expression(&self) -> Result<Vec<RollExpression>> {
-        let expr: Arc<String> = Arc::new(self.args.expression.clone());
-        let r = expr.as_ref();
-        todo!();
-        // let (_, dice_exprs) = roll_expressions_list(r)
-        //     .finish()
-        //     .map_err(|err| anyhow!("parser error: {}", err.to_string()))?;
-        // Ok(dice_exprs)
-    }
-
-    #[allow(dead_code)]
-    fn make_rolls(&self, expression: &RollExpression) -> Vec<RollResults> {
-        expression
-            .calculation
-            .dice
-            .iter()
-            .map(RollResults::from)
-            .collect::<Vec<_>>()
-    }
-
-    #[allow(dead_code)]
-    fn print_rolls(&self, rolls: &[RollResults]) {
-        for roll_res in rolls.iter() {
-            let die_rolls = roll_res
-                .results
-                .iter()
-                .map(ToString::to_string)
-                .collect::<Vec<_>>()
-                .join(", ");
-
-            println!("{die}: {rolls}", die = roll_res.name, rolls = die_rolls);
-        }
-    }
-
-    fn print_roll_expr(&self, expression: &RollExpression) {
-        let name = expression.to_string();
-        let sum = expression.calculation.roll();
-        println!("{name}: {sum}");
-    }
-
-    #[allow(dead_code)]
-    fn print_sum(&self, rolls: &[RollResults], numbers: &[u32]) {
-        let rolls_sum = rolls
-            .iter()
-            .map(|roll| roll.results.iter().sum::<u32>())
-            .sum::<u32>();
-        let nums_sum = numbers.iter().sum::<u32>();
-        let total = rolls_sum + nums_sum;
-        println!("Sum: {total}");
-    }
-
     pub fn run(&self) -> Result<()> {
-        let expressions = self.parse_expression()?;
-        for expr in expressions.iter() {
-            self.print_roll_expr(expr);
-        }
-        // let rolls = self.make_rolls(&expression);
-        // self.print_rolls(&rolls);
-
-        // if self.args.show_sum {
-        //     self.print_sum(&rolls, &expression.numbers);
-        // }
+        let expr = self.args.expression.parse::<ExprKind>()?;
+        dbg!(&expr);
+        println!("{}", format_expr(&expr)?);
 
         Ok(())
     }
