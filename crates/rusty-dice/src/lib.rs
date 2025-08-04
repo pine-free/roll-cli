8657317ece9c25112d38b829c3f156c3a13689d4
Freya Pines <one-true-cat@yandex.ru>
2025-08-04 15:08:54 +0500

    feat: added dice parse error

--- crates/rusty-dice/src/lib.rs
+++ crates/rusty-dice/src/lib.rs
@@ -1,6 +1,13 @@
-use std::{num::ParseIntError, str::FromStr};
+use std::str::FromStr;
 
 use rand::Rng;
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum DiceError {
+    #[error("Failed to parse dice expression")]
+    InvalidExpression(String),
+}
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Dice {
@@ -28,24 +35,30 @@ impl Dice {
 }
 
 impl FromStr for Dice {
-    type Err = ParseIntError;
+    type Err = DiceError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let dice_str = s.split("d").take(2).collect::<Vec<_>>();
-        let quantity = dice_str
+        let dice_parts = s.split("d").collect::<Vec<_>>();
+
+        let to_err = || DiceError::InvalidExpression(s.to_string());
+
+        if dice_parts.len() != 2 {
+            return Err(to_err());
+        }
+
+        let quantity = dice_parts
             .get(0)
             .expect("There should always be the first element in the dice label")
-            .parse::<u32>()?;
+            .parse::<u32>()
+            .map_err(|_| to_err())?;
 
-        let num_sides = dice_str
+        let num_sides = dice_parts
             .get(1)
             .expect("There should always be the second element in the dice label")
-            .parse::<u32>()?;
+            .parse::<u32>()
+            .map_err(|_| to_err())?;
 
-        Ok(Dice {
-            num_sides,
-            quantity,
-        })
+        Ok(Dice::new(quantity, num_sides))
     }
 }
 
@@ -73,5 +86,15 @@ mod tests {
             .parse::<Dice>()
             .expect("Expression should be parseable");
         assert_eq!(die, Dice::new(4, 8));
+    }
+
+    #[test]
+    fn parse_err() {
+        let test_cases = ["3d5d8d9", "-10d8", "whatdochat", "lolkek"].map(str::to_string);
+
+        for test in test_cases {
+            let res = test.parse::<Dice>();
+            assert_eq!(res, Err(DiceError::InvalidExpression(test)));
+        }
     }
 }
