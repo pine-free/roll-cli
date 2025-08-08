d1594bf008afb24471c00b40606395bef47b4c0f
Freya Pines <one-true-cat@yandex.ru>
2025-08-08 23:07:22 +0500

    feat: added parsing for cards from cli

--- crates/rusty-roll-tables/src/parse.rs
+++ crates/rusty-roll-tables/src/parse.rs
@@ -1,5 +1,14 @@
-use nom::{IResult, Parser, character::complete::digit1, combinator::map, error::Error};
-use rusty_dice::cards::Card;
+use std::str::FromStr;
+
+use nom::{
+    IResult, Parser,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{digit1, one_of},
+    combinator::{map, recognize},
+    error::{Error, ParseError},
+};
+use rusty_dice::cards::{Card, CardType, Suit};
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Atom {
@@ -12,4 +21,52 @@ type ParseRes<'a, T> = IResult<&'a str, T, Error<&'a str>>;
 
 fn parse_number(i: &str) -> ParseRes<i32> {
     map(digit1, |num_str: &str| num_str.parse().unwrap()).parse(i)
+}
+
+macro_rules! simple_parser {
+    ($name:ident, $parser:expr, $target:tt) => {
+        fn $name(i: &str) -> ParseRes<$target> {
+            map($parser, |parsed_str: &str| {
+                parsed_str.parse::<$target>().unwrap()
+            })
+            .parse(i)
+        }
+    };
+}
+
+simple_parser!(parse_suit, recognize(one_of("SDCH")), Suit);
+simple_parser!(
+    parse_card_type,
+    alt((recognize(one_of("AJQK23456789")), tag("10"))),
+    CardType
+);
+
+fn parse_card(i: &str) -> ParseRes<Card> {
+    map((parse_card_type, parse_suit), |(card_type, suit)| {
+        Card::new(card_type, suit)
+    })
+    .parse(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! simple_test {
+        ($name:ident, $parser:ident, $input:literal, $target:expr) => {
+            #[test]
+            fn $name() -> () {
+                let val = $input;
+                let (_, val) = $parser(val).unwrap();
+                assert_eq!(val, $target);
+            }
+        };
+    }
+
+    simple_test!(
+        test_parse_card,
+        parse_card,
+        "AS",
+        Card::new(CardType::Ace, Suit::Spades)
+    );
 }
