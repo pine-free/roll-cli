0af38e9d8341c45ac88357e6801f546792461b20
Freya Pines <one-true-cat@yandex.ru>
2025-08-08 21:22:29 +0500

    feat: moved language stuff into data module

---
+++ crates/rusty-roll-tables/src/data.rs
@@ -0,0 +1,38 @@
+use std::{collections::HashMap, iter::Step};
+
+pub(crate) trait Key: Eq + std::hash::Hash {}
+
+impl<T> Key for T where T: Eq + std::hash::Hash {}
+
+#[derive(Clone, Debug, Default)]
+pub struct RollTable<K, V>
+where
+    K: Key,
+{
+    pub(crate) storage: HashMap<K, V>,
+}
+
+impl<K: Key, V> RollTable<K, V> {
+    pub(crate) fn inner_mut(&mut self) -> &mut HashMap<K, V> {
+        &mut self.storage
+    }
+
+    pub(crate) fn inner(&self) -> &HashMap<K, V> {
+        &self.storage
+    }
+}
+
+impl<K: Key, V> RollTable<K, V>
+where
+    K: Step,
+    V: Clone,
+{
+    pub(crate) fn insert_iter<I>(&mut self, k: I, v: V)
+    where
+        I: Iterator<Item = K>,
+    {
+        for key in k.collect::<Vec<_>>().into_iter() {
+            self.storage.insert(key, v.clone());
+        }
+    }
+}
--- crates/rusty-roll-tables/src/lib.rs
+++ crates/rusty-roll-tables/src/lib.rs
@@ -1,49 +1,26 @@
 #![feature(step_trait)]
 
-use std::{collections::HashMap, iter::Step};
-
-trait Key: Eq + std::hash::Hash {}
-
-impl<T> Key for T where T: Eq + std::hash::Hash {}
-
-#[derive(Clone, Debug, Default)]
-pub struct RollTable<K, V>
-where
-    K: Key,
-{
-    storage: HashMap<K, V>,
-}
-
-impl<K: Key, V> RollTable<K, V> {
-    fn inner_mut(&mut self) -> &mut HashMap<K, V> {
-        &mut self.storage
-    }
-
-    fn inner(&self) -> &HashMap<K, V> {
-        &self.storage
-    }
-}
-
-impl<K: Key, V> RollTable<K, V>
-where
-    K: Step,
-    V: Clone,
-{
-    fn insert_range(&mut self, k: std::ops::Range<K>, v: V) {
-        for key in k.collect::<Vec<_>>().into_iter() {
-            self.storage.insert(key, v.clone());
-        }
-    }
-}
+pub mod data;
+pub use data::RollTable;
 
 #[cfg(test)]
 mod tests {
-    use crate::RollTable;
+    use crate::data::RollTable;
 
     #[test]
     fn test_insert() {
         let mut table: RollTable<i32, i32> = RollTable::default();
         table.inner_mut().insert(12, 53);
         assert_eq!(table.inner().get(&12), Some(&53));
+    }
+
+    #[test]
+    fn test_insert_range() {
+        let mut table: RollTable<i32, i32> = RollTable::default();
+        let val = 42;
+        table.insert_iter(1..=10, val);
+        for k in 1..=10 {
+            assert_eq!(table.inner().get(&k), Some(&val));
+        }
     }
 }
