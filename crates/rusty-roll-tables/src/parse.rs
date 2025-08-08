93cd3bf54cea83591f2d575ae8311751d06f3c80
Freya Pines <one-true-cat@yandex.ru>
2025-08-08 21:27:57 +0500

    feat: added num parsing

--- crates/rusty-roll-tables/src/parse.rs
+++ crates/rusty-roll-tables/src/parse.rs
@@ -1,8 +1,15 @@
+use nom::{IResult, Parser, character::complete::digit1, combinator::map, error::Error};
 use rusty_dice::cards::Card;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Atom {
-    RollResult(i32),
+    Number(i32),
     Card(Card),
     Text(String),
+}
+
+type ParseRes<'a, T> = IResult<&'a str, T, Error<&'a str>>;
+
+fn parse_number(i: &str) -> ParseRes<i32> {
+    map(digit1, |num_str: &str| num_str.parse().unwrap()).parse(i)
 }
